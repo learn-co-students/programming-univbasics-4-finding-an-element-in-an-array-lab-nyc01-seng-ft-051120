8f4248c89095184f3305dc80d7d1eee730515ef7
Chase <chaserabenn@gmail.com>
2020-05-05 01:56:49 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,15 @@
+#def find_element_index(array, value_to_find)
+#  array.index(value_to_find)
+#end
+
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+  count = 0
+
+  while count < array.length
+    if array[count] == value_to_find
+      return count
+    else
+      count += 1
+    end
+  end
 end
